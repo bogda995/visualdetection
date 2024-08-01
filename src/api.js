cb5a7d9d9a89edf8811f84248a5d114494050592
Bogdan Man <bogda995@gmail.com>
2024-08-01 22:03:34 +0300

    adding in api js the react app backend url from render

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
-// src/api.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/', // The base URL for your Django API
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:8000/', // Use environment variable or fallback to local
 });
 
 export default instance;
