57ba467550cf1a75a938886708857af39d56e793
Bogdan Man <bogda995@gmail.com>
2024-08-02 19:43:16 +0300

    change text above input

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <>
       <div>
-        <p className="center white f3">{"This will detect faces in your pictures"}</p>
+        <p className="center white f3">{"Introduce a valid url link with a image"}</p>
       </div>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
